75140b70534db399cafe251446046f1299eb0bac
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-01-20 22:45:27 -0300

    Renderizaçao condicional

--- src/RandomDogApiStateful.js
+++ src/RandomDogApiStateful.js
@@ -6,8 +6,9 @@ class RandomDogApi extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: "Await please!",
+      message: undefined,
       status: "Loading...",
+      isLoaded: false,
     };
   }
 
@@ -18,6 +19,7 @@ class RandomDogApi extends React.Component {
         this.setState({
           message: data.message,
           status: data.status,
+          isLoaded: true,
         });
       });
   }
@@ -26,17 +28,34 @@ class RandomDogApi extends React.Component {
     this.fetchRandomDogAPI();
   }
 
+  renderLoading() {
+    return <p>Loading</p>;
+  }
+
+  handleCick() {
+    this.setState({
+      message: undefined,
+      status: "Loading...",
+      isLoaded: false,
+    });
+    this.fetchRandomDogAPI();
+  }
+
   render() {
     return (
       <div>
         <div id="random-dog-image">
-          <img
-            src={this.state.message}
-            alt="Random Dog Api"
-            width="300px"
-            height="300px"
-          />
-          <button id="new-random-dog" onClick={() => this.fetchRandomDogAPI()}>
+          {this.state.message ? (
+            <img
+              src={this.state.message}
+              alt="Random Dog Api"
+              width="300px"
+              height="300px"
+            />
+          ) : (
+            this.renderLoading()
+          )}
+          <button id="new-random-dog" onClick={() => this.handleCick()}>
             New Dog
           </button>
         </div>
